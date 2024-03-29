24a55ea0e3bd27cd46ef9423c235c86678b3f51f
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-29 21:21:17 +0800

    feat(菜鸟): 全屏广告-弹窗广告（补充规则）

--- src/apps/com.cainiao.wireless.ts
+++ src/apps/com.cainiao.wireless.ts
@@ -7,7 +7,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '弹窗广告',
+      name: '全屏广告-弹窗广告',
       rules: [
         {
           key: 0,
@@ -28,43 +28,52 @@ export default defineAppConfig({
             'View[childCount=4] > Image + View + Image + View[text.length>0][clickable=true]',
           snapshotUrls: 'https://i.gkd.li/i/13042279',
         },
+        {
+          key: 2,
+          quickFind: true,
+          activityIds:
+            'com.cainiao.wireless.homepage.view.activity.HomePageActivity',
+          matches: '[vid="dialog_full_image_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/13842492',
+        },
+        {
+          key: 3,
+          activityIds:
+            'com.cainiao.wireless.homepage.view.activity.HomePageActivity',
+          matches: 'View[childCount=2][clickable=true] > [text="关闭"]',
+          snapshotUrls: 'https://i.gkd.li/i/14033859',
+        },
       ],
     },
     {
       key: 2,
-      name: '包裹页面-地图底部横条广告',
-      activityIds: [
-        'com.taobao.cainiao.logistic.ui.view.LogisticDetailActivity',
-      ],
+      name: '局部广告-包裹页面-地图底部横条广告',
+      activityIds: 'com.taobao.cainiao.logistic.ui.view.LogisticDetailActivity',
       rules: 'View[desc="立即查看"] +2 ImageView[id=null]',
-      snapshotUrls: ['https://i.gkd.li/i/12914450'],
+      snapshotUrls: 'https://i.gkd.li/i/12914450',
     },
     {
       key: 4,
-      name: '升级提示',
-
+      name: '更新提示',
       quickFind: true,
-      activityIds: [
+      activityIds:
         'com.cainiao.wireless.homepage.view.activity.HomePageActivity',
-      ],
       rules: '[text="确认"] - [text="取消"]',
-      snapshotUrls: ['https://i.gkd.li/i/13042207'],
+      snapshotUrls: 'https://i.gkd.li/i/13042207',
     },
     {
       key: 5,
-      name: '开启消息弹窗',
+      name: '权限提示-通知权限',
       desc: '自动点击关闭。',
-
       quickFind: true,
-      activityIds: [
+      activityIds:
         'com.cainiao.wireless.homepage.view.activity.HomePageActivity',
-      ],
       rules: '[id="com.cainiao.wireless:id/gg_dialog_base_close"]',
-      snapshotUrls: ['https://i.gkd.li/i/13068573'],
+      snapshotUrls: 'https://i.gkd.li/i/13068573',
     },
     {
       key: 6,
-      name: '请求好评弹窗',
+      name: '评价提示',
       activityIds: 'com.taobao.cainiao.logistic.ui.view.LogisticDetailActivity',
       rules: '@[text="取消"] + [text="好评"]',
       snapshotUrls: 'https://i.gkd.li/i/13692761',
