e5db30485e6c71ed657a4bc6b8a437862266a205
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-29 20:28:08 +0800

    feat(麦当劳): 功能类-关闭 [开通免密支付] 弹窗

--- src/apps/com.mcdonalds.gma.cn.ts
+++ src/apps/com.mcdonalds.gma.cn.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '开启通知服务',
+      name: '权限提示-通知权限',
       desc: '选择[取消]',
       activityIds: 'com.mcdonalds.gma.cn.activity.MainActivity',
       rules: '[id="com.mcdonalds.gma.cn:id/tv_cancel"][text="取消"]',
@@ -14,13 +14,27 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '优惠券弹窗',
+      name: '全屏广告-优惠券弹窗',
       desc: '自动关闭',
       rules: [
         {
           activityIds: 'com.mcdonalds.gma.cn.activity.MainActivity',
           matches: '[id="com.mcdonalds.gma.cn:id/iv_ad_close"]',
           snapshotUrls: 'https://i.gkd.li/i/13465873',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '功能类-关闭 [开通免密支付] 弹窗',
+      rules: [
+        {
+          quickFind: true,
+          activityIds: 'com.mcd.order.activity.OrderDetailActivity',
+          matches: '@[vid="iv_close"] - * >2 [text*="免密支付"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/4b26dc9e-06d9-43a2-a105-6fb711c9aedb',
+          snapshotUrls: 'https://i.gkd.li/i/14798873',
         },
       ],
     },
