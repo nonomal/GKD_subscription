4ccac4ac1c7d5b29147cc2b8cab1b451f72df402
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-30 14:12:44 +0800

    feat(南方航空): 功能类-长辈版弹窗

--- src/apps/com.csair.mbp.ts
+++ src/apps/com.csair.mbp.ts
@@ -5,12 +5,23 @@ export default defineAppConfig({
   name: '南方航空',
   groups: [
     {
+      key: 1,
+      name: '功能类-长辈版弹窗',
+      desc: '点击【暂不开启】',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      activityIds: 'com.csair.mbp.CSMBPActivity',
+      rules: '[text="开启长辈版"] -2 [text="暂不开启"]',
+      snapshotUrls: 'https://i.gkd.li/i/14810700',
+    },
+    {
       key: 3,
-      name: '首页-弹窗广告',
+      name: '全屏广告-首页-弹窗广告',
       quickFind: true,
       activityIds: 'com.csair.mbp.CSMBPActivity',
       rules: '[id="com.csair.mbp:id/close_btn"]',
-      snapshotUrls: ['https://i.gkd.li/i/13197497'],
+      snapshotUrls: 'https://i.gkd.li/i/13197497',
     },
   ],
 });
