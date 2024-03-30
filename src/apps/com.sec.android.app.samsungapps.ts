cbf46d89353ba6934d03b01746eee03e730b05dc
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-30 13:44:41 +0800

    feat(三星应用商店): 开屏广告

--- src/apps/com.sec.android.app.samsungapps.ts
+++ src/apps/com.sec.android.app.samsungapps.ts
@@ -5,6 +5,22 @@ export default defineAppConfig({
   name: '应用商店',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      rules: [
+        {
+          key: 0,
+          action: 'clickCenter',
+          quickFind: true,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          matchTime: 10000,
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/14810528',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '功能类-年满18岁',
       desc: '点击【是】',
