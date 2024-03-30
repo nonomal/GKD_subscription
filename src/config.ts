07793f44762b684a5530c22601d8996a208b65e9
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-31 01:00:47 +0800

    feat: fool

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ import { RawSubscription } from '@gkd-kit/api';
 const subsConfig: RawSubscription = {
   id: 825,
   version: 0,
-  name: 'Adpro的GKD订阅',
-  author: 'Adpro - AdproTeam',
+  name: '阅订DKGの0rpdA',
+  author: 'Adpro(AdproTeam)',
   supportUri: 'https://github.com/Adpro-Team/GKD_subscription/issues',
   checkUpdateUrl: './Adpro_gkd.version.json5', // https://github.com/orgs/gkd-kit/discussions/558
   globalGroups,
