5bc97de3bd356c24cb9c5d9fa06825de3c796453
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-31 18:43:51 +0800

    feat(会计云课堂): 开屏广告

---
+++ src/apps/com.dongao.kaoqian.phone.ts
@@ -0,0 +1,18 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.dongao.kaoqian.phone',
+  name: '会计云课堂',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: '[vid="tv_welcome_guide"]',
+      snapshotUrls: 'https://i.gkd.li/i/14809185',
+    },
+  ],
+});
