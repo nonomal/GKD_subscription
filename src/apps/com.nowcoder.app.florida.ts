56c24caac942167dbd71c8320ae04ec9c6fa8f8e
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-04 00:13:51 +0800

    feat(牛客): 开屏广告（补充规则）

--- src/apps/com.nowcoder.app.florida.ts
+++ src/apps/com.nowcoder.app.florida.ts
@@ -7,17 +7,25 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      actionCdKey: 0,
       actionMaximumKey: 0,
       rules: [
         {
           key: 0,
           matches:
-            '@View <2 FrameLayout[childCount=3] <2 FrameLayout[childCount=2] < FrameLayout[childCount=1] < [vid="fl_splash_container"]',
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [id="android:id/content"]',
           snapshotUrls: 'https://i.gkd.li/i/13855443',
+        },
+        {
+          key: 1,
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14819443',
+            'https://i.gkd.li/i/14845692',
+          ],
         },
       ],
     },
