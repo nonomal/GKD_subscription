d69dcba48db9433f12b898f14672a9c066935857
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-05 15:20:28 +0800

    feat(快手极速版): 全屏广告-红包弹窗

---
+++ src/apps/com.kuaishou.nebula.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.kuaishou.nebula',
+  name: '快手极速版',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告-红包弹窗',
+      rules: [
+        {
+          key: 1,
+          quickFind: true,
+          activityIds: 'com.yxcorp.gifshow.HomeActivity',
+          matches: '@ImageView[clickable=true] -2 [text$="免费红包"]',
+          exampleUrls:
+            'https://m.gkd.li/101449500/f7bbd1db-f519-4ff9-96cb-4cb5b2f483a2',
+          snapshotUrls: 'https://i.gkd.li/i/14879912',
+        },
+      ],
+    },
+  ],
+});
