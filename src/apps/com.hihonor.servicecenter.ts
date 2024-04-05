4e229fc7003af4476112a25adec7a04d042b0d88
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-06 00:37:08 +0800

    feat(荣耀快服务): 局部广告-首页-右侧浮窗广告

---
+++ src/apps/com.hihonor.servicecenter.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.hihonor.servicecenter',
+  name: '荣耀快服务',
+  groups: [
+    {
+      key: 1,
+      name: '局部广告-首页-右侧浮窗广告',
+      desc: '点击x',
+      quickFind: true,
+      activityIds: 'com.hihonor.servicecardcenter.activity.LauncherActivity',
+      rules: '[vid="float_cancel"]',
+      exampleUrls:
+        'https://m.gkd.li/101449500/3e41e790-2206-4780-bdc3-6a8f152c2622',
+      snapshotUrls: 'https://i.gkd.li/i/14887872',
+    },
+  ],
+});
