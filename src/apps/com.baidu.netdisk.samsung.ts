00b9e32aba5862118b86328bf94423c4e8af1e47
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-06 20:14:31 +0800

    feat: 百度网盘-三星版本

--- src/apps/com.baidu.netdisk.samsung.ts
+++ src/apps/com.baidu.netdisk.samsung.ts
@@ -3,30 +3,28 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'com.baidu.netdisk.samsung',
   name: '百度网盘-三星版本',
-  deprecatedKeys: [0],
+  deprecatedKeys: [0, 3, 4],
   groups: [
     {
       key: -1,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       actionMaximumKey: 0,
-      actionCdKey: 0,
       rules: [
         {
           key: 0,
-          quickFind: true,
           matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
-        },
-        {
-          key: -1,
-          matches:
-            '[childCount=0][visibleToUser=true][(text.length<10 && (text*="跳过" || text*="跳過" || text*="skip" || text*="Skip")) || id$="tt_splash_skip_btn" || vid*="skip" || vid*="Skip" || desc*="跳过" || desc*="skip" || (vid*="count" && vid*="down" && vid!*="countdown" && vid!*="load" && vid!*="add" && vid!*="ead" && vid!*="time")]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/e88a7ab4-0aeb-4472-a49b-3252da21ae33',
+          snapshotUrls: 'https://i.gkd.li/i/12738323',
         },
         {
           key: 1,
-          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          matches:
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [id="android:id/content"]',
           exampleUrls:
             'https://m.gkd.li/57941037/7f1babd6-7da3-4733-aca9-80ddf3c0a84e',
           snapshotUrls: 'https://i.gkd.li/i/14596658',
@@ -35,37 +33,94 @@ export default defineAppConfig({
     },
     {
       key: 1,
-      name: '弹窗广告',
-      desc: '点击右上角x图标按钮',
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
       activityIds: 'com.baidu.netdisk.ui.MainActivity',
-      rules:
-        '[id="com.baidu.netdisk.samsung:id/cl_root"] > [id="com.baidu.netdisk.samsung:id/iv_close"]',
-      snapshotUrls: 'https://i.gkd.li/i/12738331',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          matches: '[id="com.baidu.netdisk.samsung:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/12738331',
+        },
+      ],
     },
     {
       key: 2,
-      name: '关闭购买套餐卡片广告',
+      name: '局部广告-卡片广告',
+      desc: '点击关闭',
       activityIds: 'com.baidu.netdisk.ui.MainActivity',
-      rules:
-        '[id="com.baidu.netdisk.samsung:id/buy_product_root"] > [id="com.baidu.netdisk.samsung:id/iv_close"]',
-      snapshotUrls: 'https://i.gkd.li/i/12738388',
+      rules: [
+        {
+          key: 0,
+          quickFind: true,
+          matches:
+            '[id="com.baidu.netdisk.samsung:id/buy_product_root"] > [id="com.baidu.netdisk.samsung:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/i/12738388',
+        },
+        {
+          key: 1,
+          activityIds: 'com.baidu.netdisk.ui.MainActivity',
+          matches:
+            'View[childCount=2] > @TextView[text=""][clickable=true] + TextView[text="专属福利"][clickable=false]',
+          snapshotUrls: 'https://i.gkd.li/i/12738404',
+        },
+        {
+          key: 2,
+          activityIds: 'com.baidu.netdisk.ui.MainActivity',
+          matches:
+            '@TextView[text=""][clickable=true] - TextView[text="去使用"][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/12738449',
+        },
+      ],
     },
     {
-      key: 3,
-      name: '关闭[专属福利]卡片广告',
-      activityIds: 'com.baidu.netdisk.ui.MainActivity',
-      rules:
-        'View[childCount=2] > @TextView[text=""][clickable=true] + TextView[text="专属福利"][clickable=false]',
-      snapshotUrls: 'https://i.gkd.li/i/12738404',
+      key: 5,
+      name: '评价提示',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[vid="view_score_style"] > [text="以后再说"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/167a8392-23c8-4e84-a55f-4f91e7e560d8',
+          snapshotUrls: 'https://i.gkd.li/i/14893286',
+        },
+      ],
     },
     {
-      key: 4,
-      name: '关闭[今日福利]卡片广告',
-      desc: '会员-我的会员-底部卡片广告,点击[去使用]右侧的x关闭图标',
-      activityIds: 'com.baidu.netdisk.ui.MainActivity',
-      rules:
-        '@TextView[text=""][clickable=true] - TextView[text="去使用"][clickable=true]',
-      snapshotUrls: 'https://i.gkd.li/i/12738449',
+      key: 6,
+      name: '通知提示',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[vid="dialog_cancel"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/06ae328b-f852-407b-83ab-ec295d41baa4',
+          snapshotUrls: 'https://i.gkd.li/i/14893280',
+        },
+      ],
+    },
+    {
+      key: 7,
+      name: '功能类-[上新【专辑】功能]弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          quickFind: true,
+          activityIds:
+            'com.baidu.netdisk.filealbum.ui.activity.FileAlbumListActivity',
+          matches: '[vid="add_widget_dialog_close"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/256b5cad-a5ad-48e3-ba72-9c84447f2cac',
+          snapshotUrls: 'https://i.gkd.li/i/14893285',
+        },
+      ],
     },
   ],
 });
