5e2802b05c049d381297fd1e66dbd014c994eef0
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-07 22:44:24 +0800

    feat(阿里云盘): 全屏广告-签到任务完成弹窗

--- src/apps/com.alicloud.databox.ts
+++ src/apps/com.alicloud.databox.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '自动签到',
+      name: '功能类-自动签到',
       activityIds: 'com.alicloud.databox.MainActivity',
       rules: [
         {
@@ -28,14 +28,14 @@ export default defineAppConfig({
     },
     {
       key: 1,
-      name: '活动弹窗',
+      name: '全屏广告-活动弹窗',
       activityIds: 'com.alicloud.databox.MainActivity',
       rules: '[text^="好运盲盒"] >n View > Image + TextView[clickable=true]',
       snapshotUrls: 'https://i.gkd.li/i/13228610',
     },
     {
       key: 2,
-      name: '时光设备间-自动点击“开心收下”',
+      name: '功能类-时光设备间-自动点击“开心收下”',
       activityIds:
         'com.alipay.mobile.nebulax.integration.mpaas.activity.NebulaActivity$Main',
       actionMaximum: 1,
@@ -46,7 +46,7 @@ export default defineAppConfig({
     },
     {
       key: 3,
-      name: '版本更新',
+      name: '更新提示',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
@@ -58,6 +58,18 @@ export default defineAppConfig({
           snapshotUrls: 'https://i.gkd.li/i/13806865',
         },
       ],
+    },
+    {
+      key: 4,
+      name: '全屏广告-签到任务完成弹窗',
+      desc: '点击x',
+      quickFind: true,
+      activityIds:
+        'com.alicloud.databox.navigation.NavigationFragmentContainerActivity',
+      rules: '[vid="layoutLuckyBottle"] >3 [vid="ivClose"]',
+      exampleUrls:
+        'https://m.gkd.li/101449500/9f0fb5fb-cb5a-4a11-9a6c-595b8795baad',
+      snapshotUrls: 'https://i.gkd.li/i/14895886',
     },
   ],
 });
