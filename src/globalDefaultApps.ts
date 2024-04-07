eb9a1172ec5b8ef4fb56ee8803d2772b90211277
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-07 23:32:33 +0800

    feat: blakclist

--- src/globalDefaultApps.ts
+++ src/globalDefaultApps.ts
@@ -62,6 +62,7 @@ export const commonAppBlackList: string[] = [
   'com.example.c001apk', // c001apk
   'dev.anilbeesetti.nextplayer', // Next Player
   'org.kde.kdeconnect_tp', // KDE Connect
+  'xyz.adproqwq.GKDTool', // GKDTool
 
   // 系统自带非系统应用
   // 小米
