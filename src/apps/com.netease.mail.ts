3132209c36b9e2b254eab2de94f415ff201b4e67
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-10 21:47:44 +0800

    fix(网易邮箱大师): 开屏广告提前触发导致失效

--- src/apps/com.netease.mail.ts
+++ src/apps/com.netease.mail.ts
@@ -8,46 +8,42 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      actionCdKey: 0,
       actionMaximumKey: 0,
       rules: [
         {
           key: 0,
-          quickFind: true,
-          matches: '[text*="跳过"][text.length<=10]',
           excludeMatches: '[id="com.netease.mail:id/ad_skip"][clickable=false]',
+          matches: '[text*="跳过"][text.length<=10]',
           snapshotUrls: [
             'https://i.gkd.li/i/12893573',
             'https://i.gkd.li/i/12923776',
             'https://i.gkd.li/i/13195662',
             'https://i.gkd.li/i/12818335',
             'https://i.gkd.li/i/13206298', // 使用 excludeMatches 防止提前触发规则
-            'https://i.gkd.li/i/13207736', // TODO 一整块图片，无法跳过
           ],
         },
         {
           key: 1,
-          matches: '[id$="tt_splash_skip_btn"]',
-          snapshotUrls: 'https://i.gkd.li/i/12999739',
-        },
-        {
-          key: 2,
           matches:
-            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView',
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [id="android:id/content"]',
           snapshotUrls: 'https://i.gkd.li/i/14046124',
         },
         {
-          key: 3,
-          name: 'GKD1.7.0可用',
+          key: 2,
           position: {
-            right: 'width*0.1014',
-            top: 'width*0.1598',
+            left: 'width * 0.9016',
+            top: 'width * 0.1557',
           },
-          matches: '[id="com.netease.mail:id/ad_placeholder"] >3 ImageView',
-          snapshotUrls: 'https://i.gkd.li/i/12547435',
+          excludeMatches: '[text*="跳过"][text.length<=10]',
+          matches: '[id="com.netease.mail:id/ad_view"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/13207736',
+            'https://i.gkd.li/i/14900326', // 使用 excludeMatches 防止提前触发规则
+          ],
         },
       ],
     },
