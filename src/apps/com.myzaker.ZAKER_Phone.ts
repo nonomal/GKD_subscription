b7e80eab37252614bf1c1ba59c4ede09b11b86d2
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-12 19:25:21 +0800

    feat(ZAKER): 开屏广告

---
+++ src/apps/com.myzaker.ZAKER_Phone.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.myzaker.ZAKER_Phone',
+  name: 'ZAKER',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      quickFind: true,
+      rules: [
+        {
+          position: {
+            left: 'width * 0.8871',
+            top: 'width * 0.0851',
+          },
+          matches: '[vid="cover_pic"]',
+          snapshotUrls: 'https://i.gkd.li/i/14958042',
+        },
+      ],
+    },
+  ],
+});
