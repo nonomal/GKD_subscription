f5089cab0a018817e8de35e08109ff14019e29d6
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-13 11:12:41 +0800

    perf(小米商城): 全屏广告-弹窗广告

--- src/apps/com.xiaomi.shop.ts
+++ src/apps/com.xiaomi.shop.ts
@@ -6,15 +6,17 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '全屏广告-首页弹窗广告',
+      name: '全屏广告-弹窗广告',
+      desc: '点击关闭',
       activityIds: [
-        'com.xiaomi.shop.plugin.homepage.utils.AdDialog',
         'com.xiaomi.shop2.activity.MainActivity',
+        'com.xiaomi.shop.plugin.homepage.utils.AdDialog',
       ],
-      rules: '[id="android:id/content"] >3 ImageView[index=1][clickable=true]',
+      rules:
+        '[id="android:id/content"] > RelativeLayout > LinearLayout[childCount=2] > ImageView[index=1][clickable=true]',
       snapshotUrls: [
         'https://i.gkd.li/i/12649330',
-        'https://i.gkd.li/i/14393044',
+        'https://i.gkd.li/i/14393039',
       ],
     },
   ],
