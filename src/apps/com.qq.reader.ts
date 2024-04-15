61a6de1503ef3ca85f8d8965718dd7388be53bd8
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-15 20:58:15 +0800

    feat(QQ阅读): 开屏广告

--- src/apps/com.qq.reader.ts
+++ src/apps/com.qq.reader.ts
@@ -5,6 +5,22 @@ export default defineAppConfig({
   name: 'QQ阅读',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      rules: [
+        {
+          key: 0,
+          matches:
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/i/14978239',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '局部广告-右下角悬浮卡片广告',
       desc: '点击关闭',
