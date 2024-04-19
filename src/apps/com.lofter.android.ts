0b4da1b9710c6ef0b50fa3a3975ca89c3b036280
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-19 21:06:04 +0800

    feat(LOFTER): 开屏广告（补充规则）

--- src/apps/com.lofter.android.ts
+++ src/apps/com.lofter.android.ts
@@ -7,12 +7,24 @@ export default defineGkdApp({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules:
-        'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView',
-      snapshotUrls: 'https://i.gkd.li/i/14204158',
+      actionMaximumKey: 0,
+      rules: [
+        {
+          key: 0,
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/15023951',
+        },
+        {
+          key: 1,
+          matches:
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView <<n [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/i/14204158',
+        },
+      ],
     },
   ],
 });
