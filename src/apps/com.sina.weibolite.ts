6c682accab23151c883e727275409cce9fc12842
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-19 20:46:52 +0800

    perf(微博极速版): 分段广告-信息流广告

--- src/apps/com.sina.weibolite.ts
+++ src/apps/com.sina.weibolite.ts
@@ -6,35 +6,43 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '信息流广告',
+      name: '分段广告-信息流广告',
       desc: '点击广告卡片右上角x图标,点击[不感兴趣]',
-      activityIds: 'com.weico.international.activity.MainFragmentActivity',
+      quickFind: true,
+      activityIds: [
+        'com.weico.international.activity.MainFragmentActivity',
+        'com.weico.international.ui.search.SearchActivity',
+      ],
       rules: [
         {
           key: 0,
           name: '点击广告卡片右上角x图标',
-          matches: '[id="com.sina.weibolite:id/item_timeline_ad_action"]',
-          snapshotUrls: 'https://i.gkd.li/i/12738110',
+          matches: '[vid="item_timeline_ad_action"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/12738110',
+            'https://i.gkd.li/i/14996673',
+          ],
         },
         {
           preKeys: 0,
           name: '点击[不感兴趣]',
           matches:
-            '@View[clickable=true][childCount=1] > TextView[text="不感兴趣"||text$="interest"]',
+            '@View[clickable=true][childCount=1] > TextView[text="不感兴趣"||text$="interest"] <<n [vid="design_bottom_sheet"]',
           snapshotUrls: [
             'https://i.gkd.li/i/12738132',
             'https://i.gkd.li/i/13727657',
+            'https://i.gkd.li/i/14996679',
           ],
         },
       ],
     },
     {
       key: 2,
-      name: 'APP评分',
+      name: '评价提示',
+      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      activityIds: 'com.weico.international.activity.MainFragmentActivity',
       rules: '[id="com.sina.weibolite:id/ed_btn_negative"]',
       snapshotUrls: 'https://i.gkd.li/i/13727728',
     },
