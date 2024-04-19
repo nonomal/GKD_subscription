2114c7a52e17570ed08c484273ed8139d471ed3e
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-19 20:21:23 +0800

    chore: update pkg version when build

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,10 @@
 import { updateDist } from '@gkd-kit/tools';
 import subscription from './check';
-import { updateReadMeMd } from './updateMd';
+import { updateReadMeMd } from '../src/updateMd';
+import { updatePackageVersion } from '../src/updatePackageVersion';
 
 await updateDist(subscription);
 
 await updateReadMeMd();
+
+await updatePackageVersion();
--- src/updateMd.ts
+++ src/updateMd.ts
@@ -1,6 +1,6 @@
 import fs from 'node:fs/promises';
 
-// 从 README.md 中解析出 GROUP_SIZE、GLOBALGROUP_SIZE 和 VERSION 的值
+// 从 README.md 中解析出 APP_SIZE、GROUP_SIZE、GLOBALGROUP_SIZE 和 VERSION 的值
 const parseReadMeMd = async () => {
   const readmePath = process.cwd() + '/dist/README.md';
   const readmeContent = await fs.readFile(readmePath, 'utf-8');
@@ -8,18 +8,20 @@ const parseReadMeMd = async () => {
   // 使用正则表达式匹配需要的值
   const groupSizeMatch = readmeContent.match(/\|应用规则\|(\d+)\|/);
   const globalGroupSizeMatch = readmeContent.match(/\|全局规则\|(\d+)\|/);
+  const appSizeMatch = readmeContent.match(/\|应用\|(\d+)\|/);
   const versionMatch = readmeContent.match(/v(\d+)/);
 
+  const APP_SIZE = appSizeMatch ? appSizeMatch[1] : '';
   const GROUP_SIZE = groupSizeMatch ? groupSizeMatch[1] : '';
   const GLOBALGROUP_SIZE = globalGroupSizeMatch ? globalGroupSizeMatch[1] : '';
   const VERSION = versionMatch ? versionMatch[1] : '';
 
-  return { GROUP_SIZE, GLOBALGROUP_SIZE, VERSION };
+  return { APP_SIZE, GROUP_SIZE, GLOBALGROUP_SIZE, VERSION };
 };
 
 // 更新 README.md 的模板内容并写入文件
 export const updateReadMeMd = async () => {
-  const { GROUP_SIZE, GLOBALGROUP_SIZE, VERSION } = await parseReadMeMd();
+  const { APP_SIZE, GROUP_SIZE, GLOBALGROUP_SIZE, VERSION } = await parseReadMeMd();
 
   const mdTemplatePath = process.cwd() + '/Template.md';
   const readmeMdPath = process.cwd() + '/README.md';
@@ -29,6 +31,7 @@ export const updateReadMeMd = async () => {
 
   // 替换模板中的占位符
   const readMeMdText = mdTemplate
+    .replace('--APP_SIZE--', APP_SIZE)
     .replace('--GROUP_SIZE--', GROUP_SIZE)
     .replace('--GLOBALGROUP_SIZE--', GLOBALGROUP_SIZE)
     .replace('--VERSION--', VERSION);
