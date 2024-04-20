2e5b9fe863885c8884c64153c468179391805419
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-20 14:08:35 +0800

    perf(交通银行): 全屏广告-首页弹窗广告（移除错误的activityId）

--- src/apps/com.bankcomm.Bankcomm.ts
+++ src/apps/com.bankcomm.Bankcomm.ts
@@ -9,14 +9,11 @@ export default defineGkdApp({
       name: '全屏广告-首页弹窗广告',
       desc: '点击x',
       quickFind: true,
-      activityIds: [
+      activityIds:
         'com.bankcomm.module.biz.home.MainActivity',
-        'com.hihonor.android.launcher.unihome.UniHomeLauncher',
-      ],
       rules: '[vid="popup_ad_image"] < * > [vid="popup_close"]',
       snapshotUrls: [
         'https://i.gkd.li/i/12671987',
-        'https://i.gkd.li/i/12745293', // activityId: 'com.hihonor.android.launcher.unihome.UniHomeLauncher'
         'https://i.gkd.li/i/15046998',
       ],
     },
