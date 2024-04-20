22a26add44eb4baa2f78f231fd27f1db5c9b27ef
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-20 13:27:46 +0800

    chore: version

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,7 @@
 import { updateDist } from '@gkd-kit/tools';
 import subscription from './check';
 import { updateReadMeMd } from '../src/updateMd';
-import { updatePackageVersion } from '../src/updatePackageVersion';
 
 await updateDist(subscription);
 
 await updateReadMeMd();
-
-await updatePackageVersion();
