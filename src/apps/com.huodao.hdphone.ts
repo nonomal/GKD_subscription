14f639c655404d9e058fea3e78e44980350db5e5
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-21 21:23:03 +0800

    feat(找靓机): 局部广告-卡片广告

--- src/apps/com.huodao.hdphone.ts
+++ src/apps/com.huodao.hdphone.ts
@@ -6,16 +6,23 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      quickFind: true,
       name: '全屏广告-超级补贴日弹窗',
       desc: '点击X',
-      rules: [
-        {
-          matches:
-            '[id="android:id/content"] >2 [id="com.huodao.hdphone:id/dialog_close"]',
-          snapshotUrls: 'https://i.gkd.li/i/13927567',
-        },
-      ],
+      quickFind: true,
+      rules:
+        '[id="android:id/content"] >2 [id="com.huodao.hdphone:id/dialog_close"]',
+      snapshotUrls: 'https://i.gkd.li/i/13927567',
+    },
+    {
+      key: 2,
+      name: '局部广告-卡片广告',
+      desc: '点击关闭',
+      quickFind: true,
+      activityIds: 'com.huodao.hdphone.mvp.view.main.NewMainActivity',
+      rules: '[vid="iv_recycle_close"]',
+      exampleUrls:
+        'https://m.gkd.li/57941037/dc1534ca-9ab1-4d7b-8da4-6c7992882c94',
+      snapshotUrls: 'https://i.gkd.li/i/15052025',
     },
   ],
 });
