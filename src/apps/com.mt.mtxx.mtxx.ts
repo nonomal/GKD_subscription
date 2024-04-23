4aa05d55735f78d3f083a681fdf8da74c2852ce2
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-23 20:04:57 +0800

    feat(美图秀秀): 局部广告-卡片广告

--- src/apps/com.mt.mtxx.mtxx.ts
+++ src/apps/com.mt.mtxx.mtxx.ts
@@ -6,13 +6,25 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '版本更新',
+      name: '更新提示',
       quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: '[text="升级新版客户端"] - [text="取消"][clickable=true]',
       snapshotUrls: 'https://i.gkd.li/i/13238352',
+    },
+    {
+      key: 2,
+      name: '局部广告-卡片广告',
+      rules: [
+        {
+          key: 0,
+          name: '所有图片页',
+          matches: '@ImageView[clickable=true] - * > [text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/i/15076408',
+        },
+      ],
     },
   ],
 });
