04b7db09b1b251fc8db1db11e4328e182358a17e
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-26 20:14:01 +0800

    perf(企查查): 全屏广告-VIP弹窗

--- src/apps/com.android.icredit.ts
+++ src/apps/com.android.icredit.ts
@@ -6,10 +6,21 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '全屏广告-开通VIP弹窗',
-      activityIds: 'com.android.icredit.ui.MainMenuActivity',
-      rules: '[text="10周年限时返场"] >3 Image',
-      snapshotUrls: 'https://i.gkd.li/i/14720494',
+      name: '全屏广告-VIP弹窗',
+      desc: '点击关闭',
+      rules: [
+        {
+          activityIds: 'com.android.icredit.ui.MainMenuActivity',
+          matches:
+            '[text$="限时返场" || text="首单有礼"] >2 View[childCount=2] > Image[visibleToUser=true]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/eb232325-12e4-495f-a9f3-5f853d5e66f9',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14720494',
+            'https://i.gkd.li/i/15112280',
+          ],
+        },
+      ],
     },
   ],
 });
