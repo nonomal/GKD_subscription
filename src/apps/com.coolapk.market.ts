53546031cc06e92f934edf7462f75b12ea1c44ee
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-30 20:36:24 +0800

    fix(酷安): 分段广告-卡片广告误触

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -55,7 +55,7 @@ export default defineGkdApp({
     },
     {
       key: 0,
-      name: '分段广告-广告卡片',
+      name: '分段广告-卡片广告',
       desc: '点击卡片右上角按钮->免广告-点击关闭->选择关闭原因-点击不感兴趣',
       quickFind: true,
       activityIds: [
@@ -68,7 +68,10 @@ export default defineGkdApp({
         {
           key: 1,
           name: '点击右上角x按钮',
-          excludeMatches: '[text="回复"] + [text="发布"]', // 避免在编辑评论时误触图片右上角关闭按钮
+          excludeMatches: [
+            '[text="回复"] + [text="发布"]',
+            '[text="创建收藏单"][visibleToUser=true]',
+          ],
           matches: '[id="com.coolapk.market:id/close_view"]',
           snapshotUrls: [
             'https://i.gkd.li/i/12707506',
@@ -77,12 +80,13 @@ export default defineGkdApp({
             'https://i.gkd.li/i/12774771',
             'https://i.gkd.li/i/13257987',
             'https://i.gkd.li/i/14996359', // 避免误触
+            'https://i.gkd.li/i/15159886', // 避免误触
           ],
         },
         {
           preKeys: [1],
           key: 2,
-          name: '去广告/免广告-点击关闭或不感兴趣',
+          name: '点击[不感兴趣]/[关闭]',
           matches: '@[text="不感兴趣" || text="关闭"] <n * > [text*="广告"]',
           snapshotUrls: [
             'https://i.gkd.li/i/14959519',
@@ -99,7 +103,6 @@ export default defineGkdApp({
             'https://i.gkd.li/i/12655713',
             'https://i.gkd.li/i/12660759',
             'https://i.gkd.li/i/12706437',
-            'https://i.gkd.li/i/13786886', // 没有id
           ],
         },
       ],
