bbb6605c0af61f6f96ac0381293dcdc5f3505602
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-05-04 21:56:39 +0800

    feat(TIM): 功能类-发送图片时自动勾选[原图]

--- src/apps/com.tencent.tim.ts
+++ src/apps/com.tencent.tim.ts
@@ -12,5 +12,15 @@ export default defineGkdApp({
       rules: '[text^="查看原图"]',
       snapshotUrls: 'https://i.gkd.li/i/15196002',
     },
+    {
+      key: 2,
+      name: '功能类-发送图片时自动勾选[原图]',
+      quickFind: true,
+      activityIds: 'com.tencent.mobileqq.activity.SplashActivity',
+      rules: '@[checked=false] + [text="原图"]',
+      exampleUrls:
+        'https://m.gkd.li/57941037/57b4e39f-ba48-4e57-a5de-3a461a583e60',
+      snapshotUrls: 'https://i.gkd.li/i/15209768',
+    },
   ],
 });
